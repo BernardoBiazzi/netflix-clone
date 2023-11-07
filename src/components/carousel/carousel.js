68d2ac2f350fdc24fec0fa3dfd2857318b5cf0c4
bernardo.biazzi <bernardo.biazzi@sitemercado.com.br>
2023-11-07 11:31:09 -0300

    including carousel actions

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,13 +1,41 @@
 /* eslint-disable jsx-a11y/alt-text */
 import './carousel.css';
+import { useRef, useState } from 'react';
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const Carousel = ({ title, items }) => {
+    const carouselRef = useRef(null);
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    const getItemWidth = () => {
+        const firstItem = document.querySelector('.carousel-item');
+        if (firstItem) {
+            const itemRect = firstItem.getBoundingClientRect();
+            return itemRect.width;
+        } else {
+            return 0;
+        }
+    } 
+
+    const handleLeft = () => {
+        const newPosition = carouselRef.current.scrollLeft - (3 * getItemWidth());
+        carouselRef.current.scrollLeft = newPosition;
+        setTimeout(() => {setScrollPosition(carouselRef.current.scrollLeft)},600);
+    };
+
+    const handleRight = () => {
+        const newPosition = 3 * getItemWidth();
+        carouselRef.current.scrollLeft = newPosition;
+        setTimeout(() => {setScrollPosition(carouselRef.current.scrollLeft)},600);
+    };
+
     return (
         <div className="carousel-container">
             <h3>{title}</h3>
-            <div className="left-button"></div>
-            <div className="right-button"></div>
-            <div className="carousel">
+            {scrollPosition > 0 && 
+            <div className="left-button" onClick={handleLeft}><FaChevronLeft/></div>}
+            <div className="right-button" onClick={handleRight}><FaChevronRight/></div>
+            <div className="carousel" ref={carouselRef}>
                 {items.map((item) => {
                     return (
                         <div key={item.id} className="carousel-item">
