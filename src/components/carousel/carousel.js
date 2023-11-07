ae5c75ce5d8ae410c2eb203396724f0d1918b6e5
bernardo.biazzi <bernardo.biazzi@sitemercado.com.br>
2023-11-07 17:06:05 -0300

    improving carousel hovers

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,12 +1,29 @@
 /* eslint-disable jsx-a11y/alt-text */
 import './carousel.css';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const Carousel = ({ title, items }) => {
     const carouselRef = useRef(null);
     const [scrollPosition, setScrollPosition] = useState(0);
     const [enableRightArrow, setEnableRightArrow] = useState(true);
+
+    const handleScrollPosition = () => {
+        setScrollPosition(carouselRef.current.scrollLeft)
+    }
+
+    useEffect(() => {
+        const carousel = carouselRef.current;
+        carousel.addEventListener('scroll', handleScrollPosition);
+        return () => {
+            carousel.removeEventListener('scroll', handleScrollPosition);
+        };
+    }, [carouselRef]);
+
+    useEffect(() => {
+        const carousel = carouselRef.current;
+        carousel.scrollLeft = getItemWidth();
+    }, []);
 
     const getItemWidth = () => {
         const firstItem = document.querySelector('.carousel-item');
@@ -22,25 +39,19 @@ const Carousel = ({ title, items }) => {
         const carousel = carouselRef.current;
         const newPosition = carousel.scrollLeft - (6 * getItemWidth());
         carousel.scrollLeft = newPosition;
-        setTimeout(() => {
-            setEnableRightArrow(true);
-            setScrollPosition(carousel.scrollLeft);
-        },600);
+        setEnableRightArrow(true);
     };
 
     const handleRight = () => {
         const carousel = carouselRef.current;
         const newPosition = enableRightArrow ? carousel.scrollLeft + (6 * getItemWidth()) : 0;
         carousel.scrollLeft = newPosition;
-        setTimeout(() => {
-            setScrollPosition(carousel.scrollLeft);
-            const scrollLimit = carousel.scrollWidth - carousel.clientWidth;
-            if (newPosition >= scrollLimit) {
-                setEnableRightArrow(false);
-            } else {
-                setEnableRightArrow(true);
-            }
-        },600);
+        const scrollLimit = carousel.scrollWidth - carousel.clientWidth;
+        if (newPosition >= scrollLimit) {
+            setEnableRightArrow(false);
+        } else {
+            setEnableRightArrow(true);
+        }
     };
 
     return (
@@ -52,7 +63,7 @@ const Carousel = ({ title, items }) => {
             <div className="carousel" ref={carouselRef}>
                 {items.map((item) => {
                     return (
-                        <div key={item.id} className="carousel-item">
+                        <div className="carousel-item">
                             <img src={item.url}></img>
                         </div>
                     );
