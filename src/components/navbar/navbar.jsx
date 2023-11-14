3e1b6bd36784034691fa76df29e1683f8ee30be1
bernardo.biazzi <bernardo.biazzi@sitemercado.com.br>
2023-11-14 16:19:15 -0300

    including click actions on navbar buttons

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 import './navbar.scss';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import logo from '../../assets/img/logo.png';
 import { FaSistrix, FaRegBell, FaRegUser } from "react-icons/fa";
@@ -35,16 +35,24 @@ const Navbar = () => {
         <NavLink to="idiomas">Navegar por idiomas</NavLink>
     </>);
 
+    const navigate = useNavigate();
+
     return (
-        <nav className={`navbar${topDistance > 10 ? ' fill-navbar' : ''}`}>
+        <nav className={`navbar ${topDistance > 10 ? 'fill-navbar' : ''}`}>
             <div className="left-side">
                 <img className="logo" src={logo} alt="Logo"/>
                 {innerWidth > 800 && navLinks}
             </div>
             <div className="right-side">
-                <button data-testid="search-button"><FaSistrix/></button>
-                <button data-testid="bell-button"><FaRegBell/></button>
-                <button data-testid="user-button"><FaRegUser/></button>
+                <button onClick={() => navigate('search')} data-testid="search-button">
+                    <FaSistrix/>
+                </button>
+                <button onClick={() => navigate('notificações')} data-testid="bell-button">
+                    <FaRegBell/>
+                </button>
+                <button onClick={() => navigate('/')} data-testid="user-button">
+                    <FaRegUser/>
+                </button>
             </div>
         </nav>
     );
